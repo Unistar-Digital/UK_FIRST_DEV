2a6e67fab9b23fe62e6225567134d6a61b7c907b
ff001a218f4f7a51d3437a35e141b382218a4d1208a908f402edeba3212e07cf07d54 (JAICP) <ff001a218f4f7a51d3437a35e141b382218a4d1208a908f402edeba3212e07cf07d54>
2024-11-07 13:44:18 +0000

    Added directories: //functions/
    Added files:
    /functions/errors.js

---
+++ functions/errors.js
@@ -0,0 +1,31 @@
+init:
+    $mail.debug(true); 
+    
+init: 
+    bind("onScriptError", function($context) {
+        $mail.send({
+            from: "just-alarm@unistar.ru",
+            to: ["a.polyakov@unistar.ru"],
+            subject: "OnScriptError. UK_FIRST_DEV",
+            content: "Возникла ошибка скрипта на проекте UK_FIRST_DEV",
+            smtpHost: "mail.unistar.ru", 
+            smtpPort: "465",
+            user: "just-alarm@unistar.ru",
+            password: "$SMTP_password"})
+        $reactions.transition({value: "/Start/Operator", deferred: true});
+    });
+
+
+init:
+    bind("onAnyError", function($context) {
+        $mail.send({
+            from: "just-alarm@unistar.ru",
+            to: ["a.polyakov@unistar.ru"],
+            subject: "OnAnyError. UK_FIRST_DEV",
+            content: "Возникла ошибка сервера на проекте UK_FIRST_DEV",
+            smtpHost: "mail.unistar.ru", 
+            smtpPort: "465",
+            user: "just-alarm@unistar.ru",
+            password: "$SMTP_password"})
+        $reactions.transition({value: "/Start/Operator", deferred: true}); 
+    })
