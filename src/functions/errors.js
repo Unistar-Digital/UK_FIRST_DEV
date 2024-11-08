6029ca34e194a5d9a36c457b34be09ea99161a0d
ff001a218f4f7a51d3437a35e141b382218a4d1208a908f402edeba3212e07cf07d54 (JAICP) <ff001a218f4f7a51d3437a35e141b382218a4d1208a908f402edeba3212e07cf07d54>
2024-11-08 09:45:40 +0000

    Edited files: /src/functions/errors.js

--- src/functions/errors.js
+++ src/functions/errors.js
@@ -29,4 +29,4 @@ init:
             user: "just-alarm@unistar.ru",
             password: "$SMTP_password"})
         })
-        $reactions.transition({value: "/Start/Operator", deferred: true});
+    $reactions.transition({value: "/Start/Operator", deferred: true});
